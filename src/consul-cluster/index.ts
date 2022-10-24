b824df9bf528e33b8f55a33480a6f723283ff929
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2022-10-24 14:38:06 +0000

    chore(deps): upgrade provider project

--- src/consul-cluster/index.ts
+++ src/consul-cluster/index.ts
@@ -264,7 +264,7 @@ export class ConsulCluster extends cdktf.TerraformResource {
       terraformResourceType: 'hcp_consul_cluster',
       terraformGeneratorMetadata: {
         providerName: 'hcp',
-        providerVersion: '0.45.0',
+        providerVersion: '0.47.0',
         providerVersionConstraint: '~> 0.45'
       },
       provider: config.provider,
