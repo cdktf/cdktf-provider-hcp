bdde028626a443fe2cfadf8f4a2c301fbb71e50c
team-tf-cdk <84392119+team-tf-cdk@users.noreply.github.com>
2022-11-15 13:42:50 +0100

    {{ steps.diff_changes.outputs.commit_message }} (#54)
    Triggered by https://github.com/cdktf/cdktf-repository-manager/actions/runs/3470398198

--- src/hvn-peering-connection/index.ts
+++ src/hvn-peering-connection/index.ts
@@ -189,7 +189,7 @@ export class HvnPeeringConnection extends cdktf.TerraformResource {
       terraformResourceType: 'hcp_hvn_peering_connection',
       terraformGeneratorMetadata: {
         providerName: 'hcp',
-        providerVersion: '0.45.0',
+        providerVersion: '0.48.0',
         providerVersionConstraint: '~> 0.45'
       },
       provider: config.provider,
