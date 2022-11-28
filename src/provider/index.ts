3d4b3089aa8d64c1d5c1776370a681906f9a1050
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2022-11-28 09:51:00 +0000

    chore(deps): Updated

--- src/provider/index.ts
+++ src/provider/index.ts
@@ -53,7 +53,7 @@ export class HcpProvider extends cdktf.TerraformProvider {
       terraformResourceType: 'hcp',
       terraformGeneratorMetadata: {
         providerName: 'hcp',
-        providerVersion: '0.48.0',
+        providerVersion: '0.49.0',
         providerVersionConstraint: '~> 0.45'
       },
       terraformProviderSource: 'hcp'
