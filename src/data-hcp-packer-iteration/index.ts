a5449af3eb9ce27dcf6f8a2555cccfc198f392e2
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2022-12-09 14:53:12 +0000

    chore(deps): Updated

--- src/data-hcp-packer-iteration/index.ts
+++ src/data-hcp-packer-iteration/index.ts
@@ -135,7 +135,7 @@ export class DataHcpPackerIteration extends cdktf.TerraformDataSource {
       terraformResourceType: 'hcp_packer_iteration',
       terraformGeneratorMetadata: {
         providerName: 'hcp',
-        providerVersion: '0.50.0',
+        providerVersion: '0.51.0',
         providerVersionConstraint: '~> 0.45'
       },
       provider: config.provider,
