d9011e039dff872932ed7d706df887faec49ab18
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-03-18 10:04:08 +0000

    chore(deps): update  by cdktf/cdktf-repository-manager@9f952e0

--- src/consul-cluster-root-token/index.ts
+++ src/consul-cluster-root-token/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/hashicorp/hcp/0.83.0/docs/resources/consul_cluster_root_token
 // generated from terraform resource schema
 
