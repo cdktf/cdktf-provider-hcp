9b18ee132b141f42071d2bc8a1578a6c3e086ef7
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-05-27 08:51:52 +0000

    chore(deps): update  by cdktf/cdktf-repository-manager@5503dbc

--- src/packer-channel/index.ts
+++ src/packer-channel/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/hashicorp/hcp/0.90.0/docs/resources/packer_channel
 // generated from terraform resource schema
 
