273ffc3d3c3ffb41161689c4f54b99ac9ad59c70
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-06-17 12:34:42 +0000

    chore(deps): update  by cdktf/cdktf-repository-manager@136fa00

--- src/data-hcp-waypoint-add-on-definition/index.ts
+++ src/data-hcp-waypoint-add-on-definition/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/hashicorp/hcp/0.91.1/docs/data-sources/waypoint_add_on_definition
 // generated from terraform resource schema
 
