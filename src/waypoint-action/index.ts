734e8ec86108cc683696150803ee670d874c4fb6
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-08-11 06:00:55 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/waypoint-action/index.ts
+++ src/waypoint-action/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/hashicorp/hcp/0.94.1/docs/resources/waypoint_action
 // generated from terraform resource schema
 
