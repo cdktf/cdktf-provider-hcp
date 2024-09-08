8d7202d9ace54c261d44bacda885127ca6f7c2fe
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-09-08 06:01:43 +0000

    chore(deps): update  by cdktf/cdktf-repository-manager@4a3e507

--- src/data-hcp-packer-version/index.ts
+++ src/data-hcp-packer-version/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/hashicorp/hcp/0.95.1/docs/data-sources/packer_version
 // generated from terraform resource schema
 
