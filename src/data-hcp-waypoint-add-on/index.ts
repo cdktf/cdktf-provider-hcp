d9b73c6a1e62f12f509ad6e3b818514425d3091c
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2025-02-23 06:00:52 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/data-hcp-waypoint-add-on/index.ts
+++ src/data-hcp-waypoint-add-on/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/hashicorp/hcp/0.103.0/docs/data-sources/waypoint_add_on
 // generated from terraform resource schema
 
