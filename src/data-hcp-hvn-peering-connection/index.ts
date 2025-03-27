1126e0950515548ecdd32394632259aee34aacb4
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2025-03-27 22:04:59 +0000

    chore(deps): update  by cdktf/cdktf-repository-manager@2db01f0

--- src/data-hcp-hvn-peering-connection/index.ts
+++ src/data-hcp-hvn-peering-connection/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/hashicorp/hcp/0.104.0/docs/data-sources/hvn_peering_connection
 // generated from terraform resource schema
 
--- src/data-hcp-vault-secrets-rotating-secret/index.ts
+++ src/data-hcp-vault-secrets-rotating-secret/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/hashicorp/hcp/0.104.0/docs/data-sources/vault_secrets_rotating_secret
 // generated from terraform resource schema
 
